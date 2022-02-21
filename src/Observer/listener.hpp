2927971f222932760fa4917b8f3ba7f60e043b27
rbradt <rbradt009@gmail.com>
2022-02-20 21:13:07 -0800

    Changed listener logic

--- src/Observer/listener.hpp
+++ src/Observer/listener.hpp
@@ -2,13 +2,29 @@
 #define __LISTENER_HPP__
 
 #include "observer.hpp"
+#include <vector>
 
 class Listener {
+    private:
+        vector<Observer*>* observers;
+  
+    protected:
+        virtual void notifyObservers() = 0;
+
     public:
-        virtual ~Listener() {}
-        virtual void addObserver(Observer* obs) = 0;
-        virtual bool removeObserver(Observer* obs) = 0;
-        virtual void notifyObservers(Message* message, Observer* src) = 0;
+        Listener(): observers{new vector<Observer*>()} {}
+
+        ~Listener() { delete observers; }
+
+        void addObserver(Observer* obs) { observers->push_back(obs); }
+
+        bool removeObserver(Observer* obs) {
+            for(auto o: *observers)
+                if(o == obs) {
+                    observers->erase(o);
+                    break;
+                {
+        }
 };
 
 #endif // __LISTENER_HPP__
