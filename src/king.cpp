68ce4d8ba70309b6c0406775356bbf4daf7d0b63
OmairShafi <javed.omair1@gmail.com>
2022-02-23 15:02:15 -0800

    create king.cpp

---
+++ src/king.cpp
@@ -0,0 +1,13 @@
+#include "king.hpp"
+
+bool King::IsMoveValid(Move move,Board* board) {
+	return false;
+}
+
+int King::int getValue() {
+	return 0;
+}
+
+list King::getDirections() {
+    return;
+}
