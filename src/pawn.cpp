82e152c3d9ed533a5bd61a8ca910f7048a1fae6d
OmairShafi <javed.omair1@gmail.com>
2022-02-22 17:11:55 -0800

    create pawn.cpp

---
+++ src/pawn.cpp
@@ -0,0 +1,14 @@
+#include "pawn.hpp"
+
+bool Pawn::IsMoveValid(Move move,Board* board) {
+	return false;
+}
+
+int Pawn::int getValue() {
+	return 0;
+}
+
+list Pawn::getDirections() {
+    return;
+}
+
