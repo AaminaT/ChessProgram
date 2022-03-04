c711a0c5c22628197753c25338c41d6932af0fbc
rbradt <rbradt009@gmail.com>
2022-03-04 02:37:21 -0800

    fixing memory leak in path_iterator

--- src/Model/coordinate.hpp
+++ src/Model/coordinate.hpp
@@ -8,29 +8,29 @@ struct coordinate {
     coordinate(int row = 0, int col = 0): row{row}, col{col} {}
     coordinate(const coordinate&) = default;
 
-    coordinate& operator+(const coordinate& other) {
+    coordinate operator+(const coordinate& other) {
         row += other.row;
         col += other.col;
         return *this;
     }
 
-    coordinate& operator-(const coordinate& other) {
+    coordinate operator-(const coordinate& other) {
         row -= other.row;
         col -= other.col;
         return *this;
     }
 
-    int operator*(coordinate& other) {
+    int operator*(const coordinate& other) {
         return row*other.row + col*other.col;
     }
 
-    coordinate& operator*(const int& scalar) {
+    coordinate operator*(int scalar) {
         row *= scalar;
         col *= scalar;
         return *this;
     }
 
-    bool operator==(coordinate& other) {
+    bool operator==(const coordinate& other) {
         return row == other.row && col == other.col;
     }
 };
