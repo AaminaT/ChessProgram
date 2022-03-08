8979c0a1a4588a826900825f22cec539a909915b
Omair-Shafi <87246616+Omair-Shafi@users.noreply.github.com>
2022-03-08 11:05:29 -0800

    Update pawn.cpp

--- src/pawn.cpp
+++ src/pawn.cpp
@@ -7,37 +7,82 @@ bool Pawn::IsMoveValid(Move move,Board* board)
 	bool validMove = true;
 	coordinate orig = move->get_origin();
 	coordinate dest = move->get_destination();
-
-	int distance = 0; // squares between orig and dest
-
-	// - clear path check -
-	// 1. <--> movement is not allowed
-	if (orig.row == dest.row)
+	int distance = move->get_distance();
+	int number_turn = board->get_turn()
+		
+	//row 1 starting position
+	if (number_turn % 2 == 0) //even turn = black piece
 	{
-		return false;
-	}
-	// 2. same column - can move 1 if nothing directly infront
-	if (orig.col == dest.col)
-	{
-		distance = (orig.row < dest.row ? (dest.row - orig.row) - 1 : (orig.row - dest.row) - 1);
-		if (distance >= 0 || !(board->at(dest.row, dest.col) != ' '))
+		// 1. <--> movement is not allowed
+		if (orig.row == dest.row)
+		{
 			return false;
-	}
-	// for two space move, program will check 1 space ahead first
-	// 3. same column - can move 2 steps when at row 1 or 6 (rows: [0-7]) if nothing directly infront
-	if (orig.col == dest.col)
-	{
-		distance = (orig.row < dest.row ? (dest.row - orig.row) - 2 : (orig.row - dest.row) - 2);
-		if (distance != 0 || !(board->at(dest.row, dest.col) != ' '))
+		}
+		// 2. up movement is not allowed
+		if (orig.row > dest.row)
+		{
 			return false;
+		}
+		// 3. same column - can move 1 if nothing directly infront
+		if (orig.col == dest.col)
+		{
+			//orig.row < dest.row
+			if (distance < 1  || board->at(dest.row, dest.col) != ' ')
+				return false;
+		}
+		// for two space move, program will check 1 space ahead first
+		// 4. same column - can move 2 steps when at row 1 (rows: [0-7]) if nothing directly infront
+		if (orig.col == dest.col)
+		{
+			if (distance > 2 || board->at(dest.row, dest.col) != ' ' || orig.row != 1)
+				return false;
+		}
+		// 5. diagonals - when pawn has another piece in that adajacent diagonal destination is 1 space infront of the
+		if (orig.row != dest.row && orig.col != dest.col)
+		{
+			int distanceRow = dest.row - orig.row -1;
+			//a pawn piece can move to either column
+			int distanceCol = (orig.col < dest.col ? (dest.col - orig.col) - 1 : (orig.col - dest.col) - 1);
+			if ((distanceRow != 0 && distanceCol != 0) || board->at(dest.row, dest.col) == ' ')
+				return false;
+		}
 	}
-	// 4. diagonals - when pawn has another piece in that adajacent diagonal
-	if (orig.row != dest.row && orig.col != dest.col)
+	//row 6 starting position
+	else //odd turn = white piece
 	{
-		int distanceRow = (orig.row < dest.row ? (dest.row - orig.row) - 1 : (orig.row - dest.row) - 1);
-		int distanceCol = (orig.col < dest.col ? (dest.col - orig.col) - 1 : (orig.col - dest.col) - 1);
-		if ((distanceRow != 0 && distanceCol != 0) || !(board->at(dest.row, dest.col) == ' '))
+		// 1. <--> movement is not allowed
+		if (orig.row == dest.row)
+		{
 			return false;
+		}
+		// 2. down movement is not allowed
+		if (orig.row < dest.row)
+		{
+			return false;
+		}
+		// 3. same column - can move 1 if nothing directly infront
+		if (orig.col == dest.col)
+		{
+			//orig.row > dest.row
+			if (distance > -1 || board->at(dest.row, dest.col) != ' ')
+				return false;
+		}
+		// for two space move, program will check 1 space ahead first
+		// 4. same column - can move 2 steps when at row 6 (rows: [0-7]) if nothing directly infront
+		if (orig.col == dest.col)
+		{
+			if (distance < -2 || board->at(dest.row, dest.col) != ' ' || orig.row != 6)
+				return false;
+		}
+		// 5. diagonals - when pawn has another piece in that adajacent diagonal destination is 1 space infront of the
+		if (orig.row != dest.row && orig.col != dest.col)
+		{
+			int distanceRow = orig.col - dest.col - 1;
+			//a pawn piece can move to either column
+			int distanceCol = (orig.col < dest.col ? (dest.col - orig.col) - 1 : (orig.col - dest.col) - 1);
+			if ((distanceRow != 0 && distanceCol != 0) || board->at(dest.row, dest.col) == ' ')
+				return false;
+		}
 	}
 	return validMove;
 }
