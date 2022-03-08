57197257af3a94acf8730ed7e1ffe64bc3cb263e
rbradt <rbradt009@gmail.com>
2022-03-08 13:30:14 -0800

    started on path iterator tests

--- src/Tests/Model/board_tests.hpp
+++ src/Tests/Model/board_tests.hpp
@@ -56,7 +56,29 @@ TEST(BoardTests, PieceIteratorTest) {
 }
 
 TEST(BoardTests, PathIteratorTest) {
+Board* b = new Board();
+    std::stringstream ss;
+    std::string out1 = "RNBQKBNRPPPPPPPPpppppppprnbkqbnr";
 
+    int i = 0;
+    for(auto it = b->piece_begin(); it != b->piece_end(); it++) {
+        ASSERT_EQ((*it).piece(), out1[i]);
+        ASSERT_EQ((*it).pos().stringify(), coordinate(i/8 + (i < 16? 0: 4), i%8).stringify());
+        ASSERT_EQ((*it).isWhite(), i > 15);
+        i++;
+    }
+    EXPECT_EQ(i, 32);
+
+    delete b;
+    std::string out2 = "RNBKBNRPPPPPPPPpppppQpprnbkqbnr";
+    b = Board().move(coordinate(0,3), coordinate(6,5), 200);
+    
+    coordinate* c = nullptr;
+    for(auto it = b->piece_begin(); it != b->piece_end(); it++) {
+        ASSERT_EQ((*it).piece(), out2[i]);
+        i++;
+    }
+    EXPECT_NE(i, 32);
 }
 
 #endif
