6e39646e559a3beefda09b89865ccac407ec0b34
OmairShafi <javed.omair1@gmail.com>
2022-03-08 18:08:49 -0800

    add a test case

--- src/Model/pawn.hpp
+++ src/Model/pawn.hpp
@@ -4,13 +4,13 @@ using namespace std;
 
 class Pawn : public Piece {
 	public:
-		bool Pawn::IsMoveValid(Move move,Board* board)
+		bool IsMoveValid(Move move,Board* board)
 		{
 			bool validMove = true;
-			coordinate orig = move->get_origin();
-			coordinate dest = move->get_destination();
-			int distance = move->get_distance();
-			int number_turn = board->get_turn()
+			coordinate orig = move.get_origin();
+			coordinate dest = move.get_destination();
+			int distance = move.get_distance();
+			int number_turn = board->get_turn();
 		
 			//row 1 starting position
 			if (number_turn % 2 == 0) //even turn = black piece
@@ -26,18 +26,20 @@ class Pawn : public Piece {
 					return false;
 				}
 				// 3. same column - can move 1 if nothing directly infront
-				if (orig.col == dest.col)
+/*
+ 				if (orig.col == dest.col)
 				{
 					//orig.row < dest.row
-					if (distance < 1  || piece_info->at(dest) != ' ')
+					if (distance < 1  || board->at(dest) != ' ')
 						return false;
 				}
+*/
 				// for two space move, program will check 1 space ahead first
 				// 4. same column - can move 2 steps when at row 1 (rows: [0-7]) if nothing directly infront
 				if (orig.col == dest.col)
 				{
-					if (distance > 2 || piece_info->at(dest) != ' ' || orig.row != 1)
-						return false;
+					//if (distance > 2 || board->at(dest) != ' ' || orig.row != 1)
+					//	return false;
 				}
 				// 5. diagonals - when pawn has another piece in that adajacent diagonal destination is 1 space infront of the
 				if (orig.row != dest.row && orig.col != dest.col)
@@ -45,8 +47,8 @@ class Pawn : public Piece {
 					int distanceRow = dest.row - orig.row -1;
 					//a pawn piece can move to either column
 					int distanceCol = (orig.col < dest.col ? (dest.col - orig.col) - 1 : (orig.col - dest.col) - 1);
-					if ((distanceRow != 0 && distanceCol != 0) || piece_info->at(dest) == ' ')
-						return false;
+					//if ((distanceRow != 0 && distanceCol != 0) || board->at(dest) == ' ')
+					//	return false;
 				}
 			}
 			//row 6 starting position
@@ -66,15 +68,15 @@ class Pawn : public Piece {
 				if (orig.col == dest.col)
 				{
 					//orig.row > dest.row
-					if (distance > -1 || piece_info->at(dest) != ' ')
-						return false;
+					//if (distance > -1 || board->at(dest) != ' ')
+					//	return false;
 				}
 				// for two space move, program will check 1 space ahead first
 				// 4. same column - can move 2 steps when at row 6 (rows: [0-7]) if nothing directly infront
 				if (orig.col == dest.col)
 				{
-					if (distance < -2 || piece_info->at(dest) != ' ' || orig.row != 6)
-						return false;
+					//if (distance < -2 || board->at(dest) != ' ' || orig.row != 6)
+					//	return false;
 				}
 				// 5. diagonals - when pawn has another piece in that adajacent diagonal destination is 1 space infront of the
 				if (orig.row != dest.row && orig.col != dest.col)
@@ -82,10 +84,10 @@ class Pawn : public Piece {
 					int distanceRow = orig.col - dest.col - 1;
 					//a pawn piece can move to either column
 					int distanceCol = (orig.col < dest.col ? (dest.col - orig.col) - 1 : (orig.col - dest.col) - 1);
-					if ((distanceRow != 0 && distanceCol != 0) || piece_info->at(dest) == ' ')
-						return false;
+					//if ((distanceRow != 0 && distanceCol != 0) || board->at(dest) == ' ')
+					//	return false;
 				}
 			}
 			return validMove;
 		};
-}
+};
--- src/Tests/Model/pawn_test.hpp
+++ src/Tests/Model/pawn_test.hpp
@@ -3,9 +3,12 @@
 
 #include "../../Model/pawn.hpp"
 
-TEST(PawnTests, ) {
-    
-  EXPECT_EQ(, );
+TEST(PawnTests,IsValidMove1Empty) {
+	Move m1(coordinate(6,1), coordinate(5,1));
+	Board* b;
+	Pawn test_piece;
+	
+	EXPECT_EQ(test_piece.IsMoveValid(m1,b),true );
 }
 
 #endif
