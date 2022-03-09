8592f7c4679b2e64d3498a561e03bdfa69ea0214
Omair-Shafi <87246616+Omair-Shafi@users.noreply.github.com>
2022-03-08 16:27:35 -0800

    Update test.cpp

--- src/Tests/test.cpp
+++ src/Tests/test.cpp
@@ -3,6 +3,7 @@
 #include "Messages/move_tests.hpp"
 #include "Model/coordinate_tests.hpp"
 #include "Model/board_tests.hpp"
+#include "Model/pawn_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
