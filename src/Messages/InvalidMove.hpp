6855a5a3ad9c15cad13612ad21506c4bd4c6c4c6
OmairShafi <javed.omair1@gmail.com>
2022-03-10 01:40:27 -0800

    pass invalid move message

---
+++ src/Messages/InvalidMove.hpp
@@ -0,0 +1,26 @@
+#ifndef __MOVE_HPP__
+#define __MOVE_HPP__
+
+#include "message.hpp"
+#include "../Model/coordinate.hpp"
+
+class InvalidMove: public Message {
+    private:
+	Message* inv = new InvalidMove(*mov);
+	Move* m;
+    public:
+	InvalidMove(Move* move)
+	{
+		m = move;
+	}
+	Move* get_move()
+	{
+		return m;
+	}
+	virtual std::string info() {
+            return "Invalid Move";
+        }
+	virtual char m_type()
+	{
+            return 'i';
+        }
