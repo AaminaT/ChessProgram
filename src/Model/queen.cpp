ac8158b2033ab1e6c61b1e62368e496c9d6d61ca
portal <dylan@source.dev>
2022-03-09 18:10:40 -0800

    Utilize are_dependent() instead of are_equivalent()

--- src/Model/queen.cpp
+++ src/Model/queen.cpp
@@ -10,8 +10,8 @@ bool Queen::isMoveValid(Move* move, Board* board)
 	
 	// clear path check for vertical/horiztal/diagonal paths
 	// * only need to check for one coordinate direction (->) since paths expand in both directions (<->)
-	if (are_equivalent(coordinate(1, 0), dir) || are_equivalent(coordinate(0, 1), dir) ||
-		are_equivalent(coordinate(1, 1), dir) || are_equivalent(coordinate(-1, 1), dir))
+	if (are_dependent(coordinate(1, 0), dir) || are_dependent(coordinate(0, 1), dir) ||
+		are_dependent(coordinate(1, 1), dir) || are_dependent(coordinate(-1, 1), dir))
 	{
 		Board::path_iterator it = board->path_begin(*move);
 		while (it != board->path_end()) {
