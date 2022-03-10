32cc34a00bffc6b6bed37b300102df74de880018
portal <dylan@source.dev>
2022-03-09 18:08:54 -0800

    Utilize are_dependent() instead of are_equivalent()

--- src/Model/rook.cpp
+++ src/Model/rook.cpp
@@ -10,7 +10,7 @@ bool Rook::isMoveValid(Move* move, Board* board)
 
 	// clear path check for both vertical and horizontal paths
 	// * only need to check for one coordinate direction (->) since paths expand in both directions (<->)
-	if (are_equivalent(coordinate(1, 0), dir) || are_equivalent(coordinate(0, 1), dir)) {
+	if (are_dependent(coordinate(1, 0), dir) || are_dependent(coordinate(0, 1), dir)) {
 		Board::path_iterator it = board->path_begin(*move);
 		while (it != board->path_end()) {
 			++it;
